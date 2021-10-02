224d7fb57676f2e7ce8954a10a5a67b6494c54e4
Tanya Suglobova <tany.suglobova@gmail.com>
2021-10-03 00:21:37 +1300

    Added MainComponent

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
+import MainScreen from '../main/main-screen';
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  const offers = [
+    {
+      id: 1,
+      src: 'img/apartment-02.jpg',
+      price: 132,
+      name: 'Canal View Prinsengracht',
+      type: 'Apartment',
+    },
+  ];
+  return <MainScreen offers={offers} />;
 }
 
 export default App;
